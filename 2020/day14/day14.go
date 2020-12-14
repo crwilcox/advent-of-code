f9162345b3f3113bc07afd060359731abadc117b
Chris Wilcox <git@crwilcox.com>
2020-12-13 22:31:00 -0800

    Day 14: bitmasks, and ferries, and solar flares, oh my

---
+++ 2020/day14/day14.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func readFileToLines(path string) ([]string, error) {
+	rootDir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(filepath.Join(rootDir, path))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+// Apply a bitmask to a provided int. X indicates to do nothing, 0 and 1
+// each result in that position getting a 0 or 1 respectively
+// 010 masked with x01 => 001
+func applyBitmask(rhs int, mask string) int {
+	binaryRepresentation := strconv.FormatInt(int64(rhs), 2)
+	binaryRepresentation = fmt.Sprintf("%036v", binaryRepresentation)
+	out := ""
+	for k, v := range binaryRepresentation {
+		if mask[k] == 'X' {
+			out += string(v)
+		} else {
+			out += string(mask[k])
+		}
+	}
+
+	i, _ := strconv.ParseInt(out, 2, 64)
+	return int(i)
+}
+
+// Part 1 Mask Values, Sum Stored Values
+func maskInputAndSumStoredValuesV1(lines []string) int {
+	mem := make(map[int]int)
+	mask := "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	for _, line := range lines {
+		split := strings.Split(line, " = ")
+		lhs := split[0]
+		rhs := split[1]
+
+		if lhs == "mask" {
+			// update mask
+			mask = rhs
+		} else {
+			// store a value in memory
+			index, _ := strconv.Atoi(lhs[4 : len(lhs)-1])
+			rhs, _ := strconv.Atoi(rhs)
+			calculatedValue := applyBitmask(rhs, mask)
+			mem[index] = calculatedValue
+		}
+	}
+
+	// all values processed, sum values
+	sum := 0
+	for _, v := range mem {
+		sum += v
+	}
+	return sum
+}
+
+// Given a bitmask contianing 'X' return all possible bitmasks.
+// an "X" represents both a 1 and a 0 state
+func expandBitmasks(bitmask string) []string {
+	result := ""
+	for k, v := range bitmask {
+		if v != 'X' {
+			result += string(v)
+		} else {
+			// if we find an X, it is floating
+			// calculate the mask as both a 0 and a 1
+			arr := []byte(bitmask)
+			arr[k] = '0'
+			expansionsZero := expandBitmasks(string(arr))
+
+			arr[k] = '1'
+			expansionsOne := expandBitmasks(string(arr))
+			return append(expansionsOne, expansionsZero...)
+		}
+	}
+
+	return []string{result}
+}
+
+// Given a value (memory address), Apply the rules for bitmasks for part 2,
+// returns expanded bitmasks.
+func findFloatingBitmaskedAddresses(val int, mask string) []string {
+	binaryRepresentation := strconv.FormatInt(int64(val), 2)
+	binaryRepresentation = fmt.Sprintf("%036v", binaryRepresentation)
+
+	v2Mask := ""
+	for k, v := range binaryRepresentation {
+		if mask[k] == 'X' {
+			v2Mask += "X" // leave an x here. this let's us expand.
+		} else if mask[k] == '1' {
+			v2Mask += string(mask[k])
+		} else if mask[k] == '0' {
+			v2Mask += string(v)
+		}
+	}
+
+	// the "x" above can be expanded now to a list of precise addresses
+	bitmasks := expandBitmasks(v2Mask)
+	return bitmasks
+}
+
+// Part 2 Mask Memory Addresses, Sum Stored Values
+func maskInputAndSumStoredValuesV2(lines []string) int {
+	mem := make(map[int64]int)
+
+	mask := ""
+	for _, line := range lines {
+		split := strings.Split(line, " = ")
+		lhs := split[0]
+		rhs := split[1]
+
+		if lhs == "mask" {
+			// update mask
+			mask = rhs
+		} else {
+			// store a value in memory
+			index, _ := strconv.Atoi(lhs[4 : len(lhs)-1])
+			rhs, _ := strconv.Atoi(rhs)
+			calculatedAddresses := findFloatingBitmaskedAddresses(index, mask)
+			for _, address := range calculatedAddresses {
+				intAddress, _ := strconv.ParseInt(address, 2, 64)
+				mem[intAddress] = rhs
+			}
+		}
+	}
+
+	// all values processed, sum values
+	sum := 0
+	for _, v := range mem {
+		sum += int(v)
+	}
+	return sum
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:", os.Args[0], "path/to/input/file")
+		return
+	}
+	filePath := os.Args[1]
+	lines, err := readFileToLines(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("🎄 Part 1 🎁: ") // Answer: 17934269678453
+	fmt.Println(maskInputAndSumStoredValuesV1(lines))
+	fmt.Println("🎄 Part 2 🎁: ") // Answer: 3440662844064
+	fmt.Println(maskInputAndSumStoredValuesV2(lines))
+}
